53dbb7a218168e31df1aaeea4d1528d45a5d9e38
domin <dominik@cacic.at>
2026-05-06 19:50:32 +0200

    fix middleware conflict

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { clerkMiddleware } from "@clerk/nextjs/server"
-
-export default clerkMiddleware()
-
-export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/api/(.*)",
-  ],
-}
-=======
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
@@ -30,4 +18,3 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"]
 }
->>>>>>> d277206 (fix auth + admin + clerk)
