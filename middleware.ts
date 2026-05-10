d1a80cf704158bc8450d8792f789ccb7b0dc4085
DomiSkyX <140248230+DomiSkyX@users.noreply.github.com>
2026-05-10 14:29:58 +0200

    middleware.ts aktualisieren

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,21 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
   "/",
+  "/projects",
+  "/about",
+  "/contact",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/projects(.*)",
-  "/about",
-  "/contact"
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth()
+  const { userId } = await auth()
 
   if (!isPublicRoute(req) && !userId) {
-    return redirectToSignIn()
+    return auth().redirectToSignIn()
   }
 })
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"]
+  matcher: ["/((?!_next|.*\\..*).*)"],
 }
